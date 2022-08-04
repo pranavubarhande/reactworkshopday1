aed99ab83544b5b35a5dbe9af4cadc851de30e23
pranavubarhande <pranavubarhande@gmail.com>
2022-08-04 10:19:44 +0530

    Day1

---
+++ src/components/Classcmp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Classcmp extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This if first class component</h1>
+      </div>
+    )
+  }
+}
+
+export default Classcmp
---
+++ src/components/Fuctionalcmp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Fuctionalcmp() {
+  return (
+    <div>We have made our first functional component</div>
+  )
+}
+
+export default Fuctionalcmp
